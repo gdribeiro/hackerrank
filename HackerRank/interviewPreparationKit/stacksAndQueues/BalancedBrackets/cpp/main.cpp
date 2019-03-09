4668a7273a5d8e7d8c969c6b1b474bf342eece8a
gdribeiro <gdribeiro@inf.ufrgs.br>
2019-03-09 01:31:12 -0300

    balanced brackets Hacker Rank

---
+++ HackerRank/interviewPreparationKit/stacksAndQueues/BalancedBrackets/cpp/main.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <cstdio>
+#include <gtest/gtest.h>
+#include <stack>
+#include <string>
+
+
+using namespace std;
+
+string yes = "YES";
+string no  = "NO";
+
+
+string isBalanced(string s) {
+    std::stack<char> bs;
+    for(int i=0; i < s.size(); i++){
+        switch(s[i]) {
+            case '{':
+            case '[':
+            case '(':
+                bs.push(s[i]);
+                break;
+            case '}':
+                if(bs.empty()) return "NO"; 
+                if(bs.top() != '{') {
+                    return "NO";
+                }
+                bs.pop();
+                break;
+            case ']':
+                if(bs.empty()) return "NO";
+                if(bs.top() != '[') {
+                    return "NO";
+                }
+                bs.pop();
+                break;
+            case ')':
+                if(bs.empty()) return "NO";
+                if(bs.top() != '(') {
+                    return "NO";
+                }
+                bs.pop();
+                break;
+        }
+    }
+    if(bs.empty()) return "YES";
+    return "NO";
+}
+
+TEST(BalancedBrackets, no1){
+    string s = "}][}}(}][))]";
+    string answer = isBalanced(s);
+    EXPECT_EQ(answer, no);
+}
+TEST(BalancedBrackets, yes1){
+    string answer = isBalanced("[](){()}");
+    EXPECT_EQ(answer, yes);
+}
+TEST(BalancedBrackets, yes2){
+    string answer = isBalanced("()");
+    EXPECT_EQ(answer, yes);
+}
+TEST(BalancedBrackets, yes3){
+    string answer = isBalanced("({}([][]))[]()");
+    EXPECT_EQ(answer, yes);
+}
+TEST(BalancedBrackets, no2){
+    string answer = isBalanced("{)[](}]}]}))}(())(");
+    EXPECT_EQ(answer, no);
+}
+TEST(BalancedBrackets, no3){
+    string answer = isBalanced("([[)");
+    EXPECT_EQ(answer, no);
+}
+TEST(BalancedBrackets, whatever) {
+    string answer = isBalanced("");
+}
+
+int main(int argc, char **argv) {
+
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
