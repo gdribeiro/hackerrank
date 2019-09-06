ffb0d0282fdec4d275c2af8191271dbeafb81825
gdribeiro <gdribeiro@inf.ufrgs.br>
2019-09-06 09:18:52 -0300

    backup

---
+++ seed-cpp.1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+#include <gtest/gtest.h>
+
+
+using namespace std;
+
+TEST(TestCathegory, testName) {
+    
+}
+
+int main(int argc, char **argv) {
+
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
